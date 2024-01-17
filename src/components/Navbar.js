fc446fb6f09398504ec019d070ca45114c22d55d
John P. Mills <johnpaulmills1@gmail.com>
2024-01-17 16:20:39 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
-import github from "../img/github-icon.svg";
+import twitter from "../img/twitter-icon.svg";
 import logo from "../img/logo.svg";
 
 const Navbar = class extends React.Component {
