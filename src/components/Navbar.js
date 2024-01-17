db8debc043c971c6e37a257ec21befa50ffef85b
John P. Mills <johnpaulmills1@gmail.com>
2024-01-17 16:35:04 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Mills" style={{ width: "50px" }} />
+              <img src={logo} alt="Mills" style={{ width: "45px" }} />
             </Link>
             {/* Hamburger menu */}
             <div
